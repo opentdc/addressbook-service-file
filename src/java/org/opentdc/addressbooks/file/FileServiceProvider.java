b36031973bc61ff69340a8feee4141accbbcf232
Werner Froidevaux <wfro@users.sourceforge.net>
2015-04-03 12:16:49 +0200

    Clean-up phase 6

--- src/java/org/opentdc/addressbooks/file/FileServiceProvider.java
+++ src/java/org/opentdc/addressbooks/file/FileServiceProvider.java
@@ -23,6 +23,130 @@
  */
 package org.opentdc.addressbooks.file;
 
-public class FileServiceProvider {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
 
+import javax.servlet.ServletContext;
+
+import org.opentdc.addressbooks.AddressbookModel;
+import org.opentdc.addressbooks.ServiceProvider;
+import org.opentdc.service.exception.DuplicateException;
+import org.opentdc.service.exception.NotFoundException;
+
+public class FileServiceProvider implements ServiceProvider {
+
+	private static Map<String, AddressbookModel> data = new HashMap<String, AddressbookModel>();
+	private static final Logger logger = Logger.getLogger(ServiceProvider.class.getName());
+
+	public FileServiceProvider(
+		ServletContext context, 
+		String prefix
+	) {
+		logger.info("> FileServiceProvider()");
+	}
+	
+	private void setNewID(AddressbookModel dataObj) {
+		String _id = UUID.randomUUID().toString();
+		dataObj.setId(_id);
+	}
+
+	private void storeData(AddressbookModel dataObj) {
+		data.put(dataObj.getId(), dataObj);
+	}
+
+	private AddressbookModel getData(String id) {
+		return data.get(id);
+	}
+
+	private List<AddressbookModel> getData() {
+		return new ArrayList<AddressbookModel>(data.values());
+	}
+
+	private int getDataSize() {
+		int _retVal = 0;
+		if (data != null) {
+			_retVal = data.size();
+		}
+		return _retVal;
+	}
+
+	private void removeData(String id) {
+		data.remove(id);
+	}
+
+	@Override
+	public List<AddressbookModel> list() {
+		List<AddressbookModel> _list = getData();
+		logger.info("list() -> " + getDataSize() + " values");
+		return _list;
+	}
+
+	@Override
+	public AddressbookModel create(
+		AddressbookModel adddressbook
+	) throws DuplicateException {
+		if (getData(adddressbook.getId()) != null) {
+			throw new DuplicateException();
+		}
+
+		// TODO: do we need to validate dataObj with BeanPropertyBindingResult ?
+		// see example in LocationService
+
+		setNewID(adddressbook);
+		storeData(adddressbook);
+		return adddressbook;
+	}
+
+	@Override
+	public AddressbookModel read(
+		String id
+	) throws NotFoundException {
+		AddressbookModel addressbook = getData(id);
+		if (addressbook == null) {
+			throw new NotFoundException();
+		}
+		// response.setId(id);
+		logger.info("read(" + id + "): " + addressbook);
+		return addressbook;
+	}
+
+	@Override
+	public AddressbookModel update(
+		AddressbookModel addressbook
+	) throws NotFoundException {
+		if (getData(addressbook.getId()) == null) {
+			setNewID(addressbook);
+		}
+		storeData(addressbook);
+		return addressbook;
+	}
+
+	@Override
+	public void delete(
+		String id
+	) throws NotFoundException {
+		AddressbookModel _dataObj = getData(id);
+
+		if (_dataObj == null) {
+			throw new NotFoundException();
+		}
+		removeData(id);
+		logger.info("delete(" + id + ")");
+	}
+
+	@Override
+	public void deleteAll() {
+		data.clear();
+	}
+		
+	@Override
+	public int count(
+	) {
+		return this.getDataSize();
+	}
+	
 }
