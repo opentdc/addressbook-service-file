fa8666526307ed9cc9fac6598ebfcaef04f1e1ca
Bruno Kaiser <bruno@bkaiser.ch>
2015-04-27 23:34:42 +0200

    cleanup

--- src/java/org/opentdc/addressbooks/file/FileServiceProvider.java
+++ src/java/org/opentdc/addressbooks/file/FileServiceProvider.java
@@ -23,6 +23,16 @@
  */
 package org.opentdc.addressbooks.file;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,44 +47,34 @@ import org.opentdc.addressbooks.ServiceProvider;
 import org.opentdc.service.exception.DuplicateException;
 import org.opentdc.service.exception.NotFoundException;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
 public class FileServiceProvider implements ServiceProvider {
+	private static final String SEED_FN = "/seed.json";
+	private static final String DATA_FN = "/data.json";
+	private static File dataF = null;
+	private static File seedF = null;
 
-	private static Map<String, AddressbookModel> data = new HashMap<String, AddressbookModel>();
+	// instance variables
+	private boolean isPersistent = true;
+	private static Map<String, AddressbookModel> index = new HashMap<String, AddressbookModel>();
 	private static final Logger logger = Logger.getLogger(ServiceProvider.class.getName());
-
+	
 	public FileServiceProvider(
 		ServletContext context, 
 		String prefix
 	) {
-	}
-	
-	private void setNewID(AddressbookModel dataObj) {
-		String _id = UUID.randomUUID().toString();
-		dataObj.setId(_id);
-	}
-
-	private void storeData(AddressbookModel dataObj) {
-		data.put(dataObj.getId(), dataObj);
-	}
-
-	private AddressbookModel getData(String id) {
-		return data.get(id);
-	}
-
-	private List<AddressbookModel> getData() {
-		return new ArrayList<AddressbookModel>(data.values());
-	}
-
-	private int getDataSize() {
-		int _retVal = 0;
-		if (data != null) {
-			_retVal = data.size();
+		if (dataF == null) {
+			dataF = new File(context.getRealPath("/" + prefix + DATA_FN));
 		}
-		return _retVal;
-	}
-
-	private void removeData(String id) {
-		data.remove(id);
+		if (seedF == null) {
+			seedF = new File(context.getRealPath("/" + prefix + SEED_FN));
+		}
+		if (index.size() == 0) {
+			importJson();
+		}
 	}
 
 	@Override
@@ -84,32 +84,39 @@ public class FileServiceProvider implements ServiceProvider {
 		long position,
 		long size
 	) {
-		List<AddressbookModel> _list = getData();
-		logger.info("list() -> " + getDataSize() + " values");
+		List<AddressbookModel> _list = new ArrayList<AddressbookModel>(index.values());
+		logger.info("list() -> " + count() + " values");
 		return _list;
 	}
 
 	@Override
 	public AddressbookModel create(
-		AddressbookModel adddressbook
+		AddressbookModel addressbook
 	) throws DuplicateException {
-		if (getData(adddressbook.getId()) != null) {
-			throw new DuplicateException();
+		logger.info("create(" + addressbook + ")");
+		String _id = addressbook.getId();
+		if (_id == null || _id == "") {
+			_id = UUID.randomUUID().toString();
+		} else {
+			if (index.get(_id) != null) {
+				// object with same ID exists already
+				throw new DuplicateException();
+			}
 		}
-
-		// TODO: do we need to validate dataObj with BeanPropertyBindingResult ?
-		// see example in LocationService
-
-		setNewID(adddressbook);
-		storeData(adddressbook);
-		return adddressbook;
+		AddressbookModel _adb = new AddressbookModel();
+		_adb.setId(_id);
+		index.put(_id, _adb);
+		if (isPersistent) {
+			exportJson(dataF);
+		}
+		return _adb;
 	}
 
 	@Override
 	public AddressbookModel read(
 		String id
 	) throws NotFoundException {
-		AddressbookModel addressbook = getData(id);
+		AddressbookModel addressbook = index.get(id);;
 		if (addressbook == null) {
 			throw new NotFoundException();
 		}
@@ -123,10 +130,10 @@ public class FileServiceProvider implements ServiceProvider {
 		String id,
 		AddressbookModel addressbook
 	) throws NotFoundException {
-		if (getData(id) == null) {
+		if (index.get(id) == null) {
 			throw new NotFoundException();
 		} else {
-			storeData(addressbook);
+			index.put(addressbook.getId(), addressbook);
 			return addressbook;
 		}
 	}
@@ -135,24 +142,134 @@ public class FileServiceProvider implements ServiceProvider {
 	public void delete(
 		String id
 	) throws NotFoundException {
-		AddressbookModel _dataObj = getData(id);
+		AddressbookModel _dataObj = index.get(id);
 
 		if (_dataObj == null) {
 			throw new NotFoundException();
 		}
-		removeData(id);
+		index.remove(id);
 		logger.info("delete(" + id + ")");
 	}
 
 	@Override
 	public void deleteAll() {
-		data.clear();
+		index.clear();
 	}
 		
 	@Override
-	public int count(
-	) {
-		return this.getDataSize();
+	public int count() {
+		int _retVal = 0;
+		if (index != null) {
+			_retVal = index.size();
+		}
+		return _retVal;
 	}
 	
+	/******************************** utility methods *****************************************/
+	void importJson() {
+		ArrayList<AddressbookModel> _adbs = null;
+
+		// read the data file
+		// either read persistent data from DATA_FN
+		// or seed data from SEED_DATA_FN if no persistent data exists
+		if (dataF.exists()) {
+			logger.info("persistent data in file " + dataF.getName()
+					+ " exists.");
+			_adbs = importJson(dataF);
+		} else { // seeding the data
+			logger.info("persistent data in file " + dataF.getName()
+					+ " is missing -> seeding from " + seedF.getName());
+			_adbs = importJson(seedF);
+		}
+		// load the data into the local transient storage
+		for (AddressbookModel _addressbook : _adbs) {
+			index.put(_addressbook.getId(), _addressbook);
+		}
+		logger.info("added " + _adbs.size() + " addressbooks to index");
+
+		if (isPersistent) {
+			// create the persistent data if it did not exist
+			if (!dataF.exists()) {
+				try {
+					dataF.createNewFile();
+				} catch (IOException e) {
+					logger.severe("importJson(): IO exception when creating file "
+							+ dataF.getName());
+					e.printStackTrace();
+				}
+				exportJson(dataF);
+			}
+		}
+		logger.info("importJson(): imported " + _adbs.size() + " addressbooks objects");
+	}
+	
+	private ArrayList<AddressbookModel> importJson(File f) throws NotFoundException {
+		logger.info("importJson(" + f.getName() + "): importing AddressbookModel");
+		if (!f.exists()) {
+			logger.severe("importJson(" + f.getName()
+					+ "): file does not exist.");
+			throw new NotFoundException("File " + f.getName()
+					+ " does not exist.");
+		}
+		if (!f.canRead()) {
+			logger.severe("importJson(" + f.getName()
+					+ "): file is not readable");
+			throw new NotFoundException("File " + f.getName()
+					+ " is not readable.");
+		}
+		logger.info("importJson(" + f.getName() + "): can read the file.");
+
+		Reader _reader = null;
+		ArrayList<AddressbookModel> _adbs = null;
+		try {
+			_reader = new InputStreamReader(new FileInputStream(f));
+			Gson _gson = new GsonBuilder().create();
+
+			Type _collectionType = new TypeToken<ArrayList<AddressbookModel>>() {
+			}.getType();
+			_adbs = _gson.fromJson(_reader, _collectionType);
+			logger.info("importJson(" + f.getName() + "): json data converted");
+		} catch (FileNotFoundException e1) {
+			logger.severe("importJson(" + f.getName()
+					+ "): file does not exist (2).");
+			e1.printStackTrace();
+		} finally {
+			try {
+				if (_reader != null) {
+					_reader.close();
+				}
+			} catch (IOException e) {
+				logger.severe("importJson(" + f.getName()
+						+ "): IOException when closing the reader.");
+				e.printStackTrace();
+			}
+		}
+		logger.info("importJson(" + f.getName() + "): " + _adbs.size()
+				+ " addressbooks imported.");
+		return _adbs;
+	}
+
+	private void exportJson(File f) {
+		logger.info("exportJson(" + f.getName() + "): exporting addressbooks");
+		Writer _writer = null;
+		try {
+			_writer = new OutputStreamWriter(new FileOutputStream(f));
+			Gson _gson = new GsonBuilder().create();
+			_gson.toJson(index.values(), _writer);
+		} catch (FileNotFoundException e) {
+			logger.severe("exportJson(" + f.getName() + "): file not found.");
+			e.printStackTrace();
+		} finally {
+			if (_writer != null) {
+				try {
+					_writer.close();
+				} catch (IOException e) {
+					logger.severe("exportJson(" + f.getName()
+							+ "): IOException when closing the reader.");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 }
