7b50c3650d1cf02ff2a08b8a6018445011427889
Bruno Kaiser <bruno@bkaiser.ch>
2015-06-01 13:56:13 +0200

    cleanup in list

--- src/java/org/opentdc/addressbooks/file/FileServiceProvider.java
+++ src/java/org/opentdc/addressbooks/file/FileServiceProvider.java
@@ -198,19 +198,11 @@ public class FileServiceProvider extends AbstractFileServiceProvider<ABaddressbo
 		for (int i = 0; i < _contacts.size(); i++) {
 			if (i >= position && i < (position + size)) {
 				_contactsSelection.add(_contacts.get(i).getModel());
-				logger.info("accepting element " + i);
-			}
-			else {
-				logger.info("ignoring element " + i);
 			}
 		}
-		/*
-		for (ABcontact _c : readAddressbook(aid).getContacts()) {
-			_contacts.add(_c.getModel());
-		}
-		*/
 		// Collections.sort(_contacts, ContactModel.ContactComparator);
-		logger.info("listContacts(<" + aid + ">, <" + query + ">, <" + queryType + ">, <" + position + ">, <" + size + ">) -> " + _contactsSelection.size()
+		logger.info("listContacts(<" + aid + ">, <" + query + ">, <" + queryType + 
+				">, <" + position + ">, <" + size + ">) -> " + _contactsSelection.size()
 				+ " values");
 		return _contactsSelection;
 	}
